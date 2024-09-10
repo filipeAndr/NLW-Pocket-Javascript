1fdf95220c1d18ea9da0411d8fc124e15d6e6633
filipeAndr <filipe_andrade@ufms.br>
2024-09-10 10:11:44 -0400

    fundamentos 002

--- index.js
+++ index.js
@@ -10,3 +10,27 @@ const mensagem = "olá mundo";
 for (let i = 0; i < 5; i++) {
     console.log(mensagem);
 }
+
+// arrays, objetos
+let lista = ["filipe", "olá"];
+console.log(lista[1] + " " + lista[0]);
+
+let meta = {
+    value: "ler um livro todo mês",
+    checked: false,
+    isChecked: (infos) => {
+        console.log(infos);
+    }
+}
+
+meta.isChecked(meta.value);
+
+// funções
+let a = 3;
+let b = 5;
+function soma(a, b) {
+    return a + b;
+}
+let c = soma(a,b);
+console.log(c);
+
