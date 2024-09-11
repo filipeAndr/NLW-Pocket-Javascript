01dcbef86327f7de5ac137bc9c8c32894d09ae26
filipeAndr <filipe_andrade@ufms.br>
2024-09-11 09:19:59 -0400

    adiciona Inquire

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const start = () =>{
-    let count = 0;
-    while(count < 10){
-        console.log(count);
-        count += 1;
+    
+    while(true){
+        let opcao = "sair";
+
+        switch(opcao){
+            case "Cadastrar":
+                console.log("vamos Cadastrar");
+                break;
+            case "Listar":
+                console.log("vamos Listar");
+                break;
+            case "sair":
+                return;
+        }
     }
     
 }
